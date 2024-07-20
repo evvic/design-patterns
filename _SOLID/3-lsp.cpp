90593d185f1635f0b2f6de124608eeab97c85f29
Eric <ewic98@gmail.com>
2024-07-20 11:22:56 -0700

    init rectangle example

--- _SOLID/3-lsp.cpp
+++ _SOLID/3-lsp.cpp
@@ -1,8 +1,53 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
+using namespace boost;
+
+class Rectangle
+{
+protected:
+    int width, height;
+public:
+    Rectangle(int width, int height) : width(width), height(height) {}
+
+    int getWidth() const {
+        return width;
+    }
+
+    void setWidth(int width) {
+        Rectangle::width=width;
+    }
+
+    int getHeight() const {
+        return height;
+    }
+
+    void setHeight(int height) {
+        Rectangle::height=height;
+    }
+
+    int area() const {
+        return width * height;
+    }
+};
+
+void process(Rectangle& r)
+{
+    int w = r.getWidth();
+    r.setHeight(10);
+
+    cout << "Area: " << r.area() << endl;
+}
 
 int main() {
-    cout << "LSP" << endl;
+    cout << "Liskov Substitution Principle" << endl;
+
+    Rectangle r{3 ,4};
+    process(r);
+
     return 0;
 }
