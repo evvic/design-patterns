b770da74ef5d62a18939631e7c99f5ccf9e9d3ef
Eric <ewic98@gmail.com>
2024-07-20 21:09:54 -0700

    abtsract factory example

---
+++ factory/HotDrink.hpp
@@ -0,0 +1,40 @@
+#pragma once
+/******************************************
+ * This header contains the corresponding
+ * family of objects that inherit from
+ * HotDrink.
+ * 
+ * In ___ there will be a corresponding
+ * family of factories to construct these
+ * objects.
+ *
+ ******************************************/
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+
+
+struct HotDrink
+{
+    virtual ~HotDrink() = default;
+    virtual void prepare(int volume) = 0;
+};
+
+struct Tea : HotDrink
+{
+    void prepare(int volume) override
+    {
+        cout << "Take tea bag, boil water, pour " << volume << " ml into cup, steep, enjoy!" << endl;
+    }
+};
+
+
+struct Coffee : HotDrink
+{
+    void prepare(int volume) override
+    {
+        cout << "Grind coffee beans, boil water, pour " << volume << " ml into cup, add milk, enjoy!" << endl;
+    }
+};
---
+++ factory/HotDrinkFactory.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "HotDrink.hpp"
+
+// Abstract Factory
+struct HotDrinkFactory
+{
+    virtual unique_ptr<HotDrink> make() const = 0;
+};
+
+struct TeaFactory : HotDrinkFactory
+{
+    unique_ptr<HotDrink> make() const override
+    {
+        return make_unique<Tea>();
+    }
+};
+
+struct CoffeeFactory : HotDrinkFactory
+{
+    unique_ptr<HotDrink> make() const override
+    {
+        return make_unique<Coffee>();
+    }
+};
